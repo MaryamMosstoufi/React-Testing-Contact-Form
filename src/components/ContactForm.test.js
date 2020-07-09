46f27bff00fb4fe41855b54a177749e57741a484
MaryamMosstoufi <maryam@zooxoos.com>
2020-07-09 14:25:06 -0400

    Create ContactForm Test File

---
+++ src/components/ContactForm.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing/library/react';
+import ContactForm from './ContactForm';
+
+// type into each input
+
+// click submit button
+
+// assert that submitted form is in data
